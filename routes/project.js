3c9d910e3576e9ab3abf319e3ed26e12f8ec9652
Lea Hyun Kim <leahkim@stanford.edu>
2014-02-21 02:34:21 -0800

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,8 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
-  // query for the specific project and
-  // call the following callback
+  models.Project.find({"_id": projectID})
+        .sort('-date').exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -14,15 +13,32 @@ exports.projectInfo = function(req, res) { 
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
-  console.log(form_data);
+  var newProject = new models.Project(form_data)
+  newProject.save(afterSaving);
 
+  function afterSaving(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    }
+    res.redirect('/');
+  }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project.find({"_id": projectID})
+        .remove().exec(afterRemoving);
 
+  function afterRemoving(err, projects) {
+    if (err) {
+          console.log(err);
+          res.send(500);
+    } 
+    res.redirect('/');
+  }
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
